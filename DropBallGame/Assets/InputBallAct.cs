dd0d52fb88b054a3471a7aaa64ece996cf09ac8e
mokwongame <ictplusud@gmail.com>
2024-04-15 12:14:16 +0900

    공 색깔 바꾸기 스크립트 추가

--- DropBallGame/Assets/InputBallAct.cs
+++ DropBallGame/Assets/InputBallAct.cs
@@ -46,13 +46,15 @@ public class InputBallAct : MonoBehaviour
         // 인스턴스(instance) 만들기(instantiate)
         // 현재 게임 오브젝트의 Transform이 만든 인스턴스가 transform
         // . 의미: 클래스의 멤버(변수, 함수)에 접근하는 기호
-        Instantiate(ball, transform.position, transform.rotation);
+        GameObject newBall = Instantiate(ball, transform.position, transform.rotation); // 공을 생성(Instantiate)해서 GameObject인 newBall을 초기화(=)
+        Renderer renderer = newBall.GetComponent<Renderer>();
+        renderer.material.color = getColor();
     }
 
     Color getColor()
     {
         // switch 구문은 if, else 구문을 간소화시킴
-        switch(colorIndex)
+        switch (colorIndex)
         {
             case 0: return Color.red; // if (colorIndex == 0) {...}
             case 1: return Color.green;
