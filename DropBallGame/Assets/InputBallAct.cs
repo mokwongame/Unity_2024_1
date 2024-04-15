0cc4b27a4de0ad9a69808cadcb544c9998d4e993
mokwongame <ictplusud@gmail.com>
2024-04-15 11:55:54 +0900

    Cube 색깔 바꾸기 스크립트 추가

--- DropBallGame/Assets/InputBallAct.cs
+++ DropBallGame/Assets/InputBallAct.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class InputBallAct : MonoBehaviour
 {
     public GameObject ball;
+    public GameObject cubeColor;
+    Renderer cubeRend;
+    // red, green, blue, white, black 순으로 색깔 변화
+    int colorSize = 5; // 색깔 개수
+    int colorIndex = 0; // 현재 색깔 위치: 0, 1, 2, 3, 4(=5-1)
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        cubeRend = cubeColor.GetComponent<Renderer>(); // 오브젝트 cubeColor의 컴포넌트(Component)에 접근해서 Renderer를 반환
+        cubeRend.material.color = Color.red;
     }
 
     // 프레임마다 호출하는 함수; 프레임: 장면의 그림
@@ -21,6 +27,18 @@ public class InputBallAct : MonoBehaviour
         {
             makeBall();
         }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow)) // 왼쪽 화살표: 색깔이 역순으로 변경
+        {
+            colorIndex--; // colorIndex = colorIndex - 1
+            if (colorIndex < 0) colorIndex = colorSize - 1; // 0보다 작으면 4로 설정
+            cubeRend.material.color = getColor();
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) // 오른쪽 화살표: 색깔이 정상적으로 변경
+        {
+            colorIndex++; // colorIndex = colorIndex + 1
+            if (colorIndex >= colorSize) colorIndex = 0; // 5보다 크거나 같으면 0으로 설정
+            cubeRend.material.color = getColor();
+        }
     }
 
     void makeBall()
@@ -31,4 +49,17 @@ public class InputBallAct : MonoBehaviour
         Instantiate(ball, transform.position, transform.rotation);
     }
 
+    Color getColor()
+    {
+        // switch 구문은 if, else 구문을 간소화시킴
+        switch(colorIndex)
+        {
+            case 0: return Color.red; // if (colorIndex == 0) {...}
+            case 1: return Color.green;
+            case 2: return Color.blue;
+            case 3: return Color.white;
+            case 4: return Color.black;
+            default: return Color.black;
+        }
+    }
 }
