4cec1f4f7f762b607ed8f21e618d4ba12c672e20
mokwongame <ictplusud@gmail.com>
2024-04-29 10:33:45 +0900

    볼링 공 재생성 기능 추가

--- BowlingGame/Assets/RollBallAct.cs
+++ BowlingGame/Assets/RollBallAct.cs
@@ -8,10 +8,12 @@ public class RollBallAct : MonoBehaviour
     float rollForce = 1000.0f;
     bool bRolling = false; // 공이 굴러가는지 확인
     Rigidbody rb; // 물리학 법칙을 적용시키는 컴포넌트
+    GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = FindAnyObjectByType<GameManager>();
         rb = gameObject.GetComponent<Rigidbody>(); // gameObject는 현재 실행 중인 게임 오브젝트
     }
 
@@ -20,12 +22,20 @@ public class RollBallAct : MonoBehaviour
     {
         float xoff = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime; // 좌우 방향 움직임의 입력(-1~+1); Time.deltaTime는 화면 리프레시 시간
         transform.Translate(xoff, 0.0f, 0.0f); // x축으로 xoff만큼 이동
-        if (!bRolling)
+        if (!bRolling) // 공이 구르지(bRolling) 않다(!, not)면(if)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 rollBall();
                 bRolling = true;
+            }
+        }
+        else // 그외(else): 공이 구르면 = else if (bRolling)
+        {
+            if (transform.position.y < -5.0f) // 현재 공의 Transform 변수(인스턴스)는 transform; 공의 높이가 -5보다 작으면
+            {
+                gameManager.setMakeBall(true);
+                Destroy(gameObject); // 현재 공의 게임 오브젝트 gameObject를 파괴; 메모리 절약을 위해 꼭 호출
             }
         }
     }
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "볼링 공을 굴리는 기능 추가"
+git commit -m "볼링 공 재생성 기능 추가"
 git push -u origin master
