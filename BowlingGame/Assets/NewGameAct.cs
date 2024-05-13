0a83b6fa94220e318aa2f2da02c12aacdd3d5c35
mokwongame <ictplusud@gmail.com>
2024-05-13 11:36:27 +0900

    NewGame 씬 추가

---
+++ BowlingGame/Assets/NewGameAct.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGameAct : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("startGame", 5.0f); // 5초 뒤에 startGame() 함수 호출
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void startGame()
+    {
+        SceneManager.LoadScene("BowlingGame"); // 게임 씬을 BowlingGame으로 변경
+    }
+}
